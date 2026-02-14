260a68342e53cb5cbe3380ce02fa08de89b564a5
Vignesh LV <lvvignesh2122@gmail.com>
2026-02-14 14:23:05 +0530

    added batch normalization and dropout for regularization

--- DL_Revision/cnn_cifar10_from_scratch.py
+++ DL_Revision/cnn_cifar10_from_scratch.py
@@ -9,6 +9,7 @@ import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+import torch.nn.functional as F
 
 # =========================================================
 # DEVICE
@@ -76,39 +77,42 @@ classes = train_dataset.classes
 
 class SimpleCNN(nn.Module):
     def __init__(self):
-        super(SimpleCNN, self).__init__()
+        super().__init__()
 
-        # Convolution Layer 1
-        self.conv1 = nn.Conv2d(
-            in_channels=3,
-            out_channels=32,
-            kernel_size=3,
-            padding=1
-        )
+        # Block 1
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
 
-        # Convolution Layer 2
-        self.conv2 = nn.Conv2d(
-            in_channels=32,
-            out_channels=64,
-            kernel_size=3,
-            padding=1
-        )
+        # Block 2
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
 
-        self.relu = nn.ReLU()
+        # Block 3
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
         self.pool = nn.MaxPool2d(2, 2)
 
-        # Fully Connected Layers
-        self.fc1 = nn.Linear(64 * 8 * 8, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.dropout = nn.Dropout(0.5)
+
+        self.fc1 = nn.Linear(128 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
 
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
+        # Conv Block 1
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+
+        # Conv Block 2
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+
+        # Conv Block 3
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
 
         x = x.view(x.size(0), -1)
 
-        x = self.relu(self.fc1(x))
+        x = self.dropout(F.relu(self.fc1(x)))
+
         x = self.fc2(x)
 
         return x
